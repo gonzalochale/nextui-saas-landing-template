861330cc4802b7ea72a074ee7a26c02a64c8c818
gonzrr <miguelgchale@gmail.com>
2023-12-04 00:15:12 -0500

    Update color variables and add Next UI theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+import {nextui} from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -14,5 +17,26 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui({
+      layout: {
+        disabledOpacity: "0.3", // opacity-[0.3]
+        radius: {
+          small: "4px", // rounded-small
+          medium: "6px", // rounded-medium
+          large: "8px", // rounded-large
+        },
+        borderWidth: {
+          small: "1px", // border-small
+          medium: "2px", // border-medium
+          large: "3px", // border-large
+        },
+      },
+      themes: {
+        light: {},
+        dark: {},
+      },
+    }),
+  ],
 }
