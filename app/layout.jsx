3f4db1dcc927f76b8426090e79b89256ddc9fc8c
Gonzalo Chale <miguelgchale@gmail.com>
2024-08-03 23:48:30 -0500

    chore:setup plausible analitycs

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import PlausibleProvider from "next-plausible";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,13 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const domain = process.env.NEXT_PUBLIC_DOMAIN || "";
+  const customDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_CUSTOM_DOMAIN;
   return (
     <html lang="en">
+      <head>
+        <PlausibleProvider domain={domain} customDomain={customDomain} />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
